8dfb224b2218152fc012cd42f9f516972a05a70e
LeonardoMonti <LeonardoMonti>
2022-08-31 12:44:58 -0300

    test : add  error case

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,9 +1,13 @@
+import { Beach } from '@src/model/beach';
+
 describe('Beaches functional tests', () => {
+  beforeAll(async () => await Beach.deleteMany({}));
+
   describe('When creating a beach', () => {
     it('should create a beach with success', async () => {
       const newBeach = {
         lat: -33.792726,
-        lgn: 151.289824,
+        lng: 151.289824,
         name: 'Manly',
         position: 'E',
       };
@@ -13,6 +17,26 @@ describe('Beaches functional tests', () => {
 
       expect(response.status).toBe(201);
       expect(response.body).toEqual(objectContaining);
+    });
+
+    it('should throw 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: 'invalid_string',
+        lng: 151.289824,
+        name: 'Manly',
+        position: 'E',
+      };
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+      });
+    });
+
+    it.skip('should return 500 when there any error other than validation error', async () => {
+      //validation error
     });
   });
 });
