4bca0a1e1b58f531389224c447940c7aca202050
LeonardoMonti <LeonardoMonti>
2022-08-31 18:33:59 -0300

    feat: add controller user in setup controllers

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import { Application } from 'express';
 import * as database from '@src/database';
 import { ForecastController } from './controllers/forecast';
 import { BeachesController } from './controllers/beaches';
+import { UsersController } from './controllers/users';
 
 export class SetupServer extends Server {
   constructor(private port = 3001) {
@@ -24,8 +25,13 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     const beachesController = new BeachesController();
+    const usersController = new UsersController();
 
-    this.addControllers([forecastController, beachesController]);
+    this.addControllers([
+      forecastController,
+      beachesController,
+      usersController,
+    ]);
   }
 
   private async setupDatabase(): Promise<void> {
