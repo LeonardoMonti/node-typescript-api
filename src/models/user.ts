5a4444cf2c885efdc17fcf27029cddaf01720577
LeonardoMonti <LeonardoMonti>
2022-08-31 19:16:20 -0300

    feat: add logic schema user for case duplicated email

--- src/models/user.ts
+++ src/models/user.ts
@@ -7,6 +7,10 @@ export interface IUser {
   password: string;
 }
 
+export enum CUSTOM_VALIDATION {
+  DUPLICATED = 'DUPLICATED',
+}
+
 interface IUserModel extends Omit<IUser, '_id'>, Document {}
 
 const schema = new mongoose.Schema(
@@ -28,6 +32,15 @@ const schema = new mongoose.Schema(
       },
     },
   }
+);
+
+schema.path('email').validate(
+  async (email: string) => {
+    const emailCount = await mongoose.models.User.countDocuments({ email });
+    return !emailCount;
+  },
+  'already exists in the database.',
+  CUSTOM_VALIDATION.DUPLICATED
 );
 
 export const User = mongoose.model<IUserModel>('User', schema);
