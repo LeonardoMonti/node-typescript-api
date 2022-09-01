6ab3b64f4a82a93a4bfedade466b231e082b5cdc
LeonardoMonti <LeonardoMonti>
2022-08-31 22:08:53 -0300

    remove consoleLOG

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -74,7 +74,6 @@ describe('Users functional tests', () => {
       const response = await global.testRequest
         .post('/users/authenticate')
         .send({ email: newUser.email, password: newUser.password });
-      console.log(response.body, 'eu aqui');
 
       expect(response.body).toEqual(
         expect.objectContaining({ token: expect.any(String) })
